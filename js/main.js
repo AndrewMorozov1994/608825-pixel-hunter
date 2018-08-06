ef0a78c9c690ff6c26cafc38443b8971d62a1f47
Андрей <089ax@mail.ru>
2018-08-06 22:55:00 +0300

    first task

---
+++ js/main.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const RIGHT_ARROW = 39;
+const LEFT_ARROW = 37;
+
+const mainElement = document.querySelector(`#main`);
+
+const arrowWrapper = document.querySelector(`.arrows__wrap`);
+const nextSlide = arrowWrapper.querySelector(`.arrows__btn--next`);
+const backSlide = arrowWrapper.querySelector(`.arrows__btn--back`);
+
+const selectSlide = (element) => {
+  mainElement.innerHTML = ``;
+  mainElement.appendChild(element.cloneNode(true));
+};
+
+const screens = Array.from(document.querySelectorAll(`template`)).map((it) => it.content);
+
+let current = 0;
+const select = (index) => {
+  index = index < 0 ? screens.length - 1 : index;
+  index = index >= screens.length ? 0 : index;
+  current = index;
+  selectSlide(screens[current]);
+};
+
+document.addEventListener(`keydown`, (evt) => {
+  switch (evt.keyCode) {
+    case RIGHT_ARROW:
+      select(current + 1);
+      break;
+    case LEFT_ARROW:
+      select(current - 1);
+      break;
+  }
+});
+
+nextSlide.addEventListener(`click`, () => {
+  select(current + 1);
+});
+
+backSlide.addEventListener(`click`, () => {
+  select(current - 1);
+});
+
+select(1);
+
