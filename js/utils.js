9b7233ae2b8de365a2eb3a92ac13c8d9868f2031
Андрей <089ax@mail.ru>
2018-08-11 01:42:45 +0300

    module2-task1 часть

---
+++ js/utils.js
@@ -0,0 +1,16 @@
+
+
+const render = (template) => {
+  const wrapper = document.createElement(`div`);
+  wrapper.innerHTML = template.trim();
+  return wrapper;
+};
+
+const mainElement = document.querySelector(`#main`);
+
+const changeScreen = (element) => {
+  mainElement.innerHTML = ``;
+  mainElement.appendChild(element);
+};
+
+export {render, changeScreen};
