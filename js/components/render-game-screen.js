ae58148dafc935e747e13eb2a947dac442f7699f
AndrewMorozov1994 <35395445+AndrewMorozov1994@users.noreply.github.com>
2018-09-08 08:48:20 +0300

    Война с игровыми экранами

--- js/components/render-game-screen.js
+++ js/components/render-game-screen.js
@@ -1,84 +1,33 @@
-import {questions} from '../data/data.js';
-import renderHeader from '../tempates/render-header.js';
-import renderQuestion from '../tempates/render-question.js';
-import {render} from '../utils.js';
-import {questionTypes} from '../data/data.js';
+// import {questions} from '../data/data.js';
+// import renderHeader from '../tempates/render-header.js';
+// import renderQuestion from '../tempates/render-question.js';
+// import {render} from '../utils.js';
+// import {questionTypes} from '../data/data.js';
 import {changeScreen} from '../utils.js';
 import {state, changeLevel} from '../game-count.js';
-import {seeGreetingScreen} from '../rules.js';
+// import {seeGreetingScreen} from '../rules.js';
 import stats from '../stats.js';
-import {chooseAnswer} from '../data/answers.js';
-import renderStats from '../tempates/render-stats.js';
+// import {chooseAnswer} from '../data/answers.js';
+// import renderStats from '../tempates/render-stats.js';
+import GameView from '../view/game-screen-view.js';
 
-const renderGameScreen = (stat) => {
-  const {lives, currentQuestion, answersTextType} = stat;
-  const question = questions[currentQuestion];
+const updateGame = (stat) => {
+  const gameView = new GameView(stat);
 
-  const template = `
-  ${renderHeader(lives)}
-  <section class="game">
-    ${renderQuestion(question)}
-        ${renderStats(answersTextType)}
-      </section>
-  `;
-
-  const element = render(template);
-
-  const gameTitle = element.querySelector(`.game__task`).innerHTML;
-  const form = element.querySelector(`form`);
-  const gameAnswers = element.querySelectorAll(`input`);
-
-  switch (gameTitle) {
-
-    case questionTypes.TWO_IMG:
-      const seeGameTwo = (evt) => {
-        if ([...gameAnswers].filter((el) => el.checked).length === 2) {
-          chooseAnswer(evt, element);
-          seeNextSlide();
-        }
-      };
-      form.addEventListener(`click`, seeGameTwo);
-      break;
-
-    case questionTypes.PHOTO_OR_PAINT:
-      for (let i = 0; i < gameAnswers.length; i++) {
-        gameAnswers[i].addEventListener(`click`, (evt) => {
-          chooseAnswer(evt, element);
-          seeNextSlide();
-        });
-      }
-      break;
-
-    case questionTypes.FIND_PAINT:
-      for (let i = 0; i < gameAnswers.length; i++) {
-        gameAnswers[i].addEventListener(`click`, (evt) => {
-          chooseAnswer(evt, element);
-          seeNextSlide();
-        });
-      }
-      break;
-    default:
-      return ``;
-  }
-
-  const seeNextSlide = () => {
-    if (state.lives >= 0 && state.currentQuestion <= 9) {
-      changeScreen(renderGameScreen(state));
+  gameView.onAnswer = () => {
+    if (stat.lives >= 0 && stat.currentQuestion <= 9) {
+      changeScreen(updateGame(state));
     } else {
       changeScreen(stats(state));
     }
-    resetGame();
+    // resetGame();
   };
-
-  const backButton = element.querySelector(`.back`);
-  setTimeout(() => seeGreetingScreen(backButton), 0);
-
-  const resetGame = () => {
-    form.reset();
-  };
+  // const resetGame = () => {
+  //   form.reset();
+  // };
   state.currentQuestion = changeLevel(state.currentQuestion);
 
-  return element;
+  return gameView.element;
 };
 
-export default renderGameScreen;
+export default updateGame;
